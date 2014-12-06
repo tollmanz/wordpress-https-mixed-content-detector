66c632c41bb360e9678642bf6338b5ddfdfa93d8
Zack Tollman <tollmanz@gmail.com>
2014-12-06 01:38:54 -0800

    Remove ability to add custom report URI.
    There is really no reason that this is necessary. If the URL can be
    controlled, we can better anticipate and handle the request.

--- src/config.php
+++ src/config.php
@@ -2,14 +2,9 @@
 /**
  * Define the path to the beacon.
  *
- * By default, this will report uri to /?mcd=report. You can change the default with a constant to send the report to a
- * custom URL.
- *
  * @since 1.0.0.
  */
-if ( ! defined( 'MCD_REPORT_URI' ) ) {
-	define( 'MCD_REPORT_URI', site_url( '/?mcd=report' ) );
-}
+define( 'MCD_REPORT_URI', site_url( '/?mcd=report' ) );
 
 /**
  * Determine whether or not to monitor admin mixed content warnings.
