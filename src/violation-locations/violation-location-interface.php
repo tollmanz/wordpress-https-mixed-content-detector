4e20b69bcf6e22b952d6f99553af75f15fcd3f1b
Zack Tollman <tollmanz@gmail.com>
2015-01-11 15:07:36 -0800

    Add logic for finding raw content matches.
    Implemented the functionality for finding a blocked URI in a post's
    content. This is a WIP commit as the feature is not fully tested.

--- src/violation-locations/violation-location-interface.php
+++ src/violation-locations/violation-location-interface.php
@@ -3,5 +3,5 @@
 interface MCD_Violation_Location {
 	public function get_location_name();
 	public function get_location_id();
-	public function search( $blocked_uri, $type );
+	public function result( $violation );
 }
